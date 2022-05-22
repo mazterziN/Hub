a240cfed25aece982a895db23b93f9fab173677b
mazterziN <105863040+mazterziN@users.noreply.github.com>
2022-05-22 06:26:15 -0400

    Update HubUI.lua

--- HubUI.lua
+++ HubUI.lua
@@ -280,7 +280,7 @@ function Kavo.CreateLib(kavName, themeList)
     subTitle.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
     subTitle.BackgroundTransparency = 1.000
     subTitle.BorderSizePixel = 0
-    subTitle.Position = UDim2.new(0.0471428565, 0, 0.344827592, 0)
+    subTitle.Position = UDim2.new(0.1471428565, 0, 0.344827592, 0)
     subTitle.Size = UDim2.new(0, 204, 0, 8)
     subTitle.Font = Enum.Font.Gotham
     subTitle.RichText = true
