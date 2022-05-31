a73a5efa3117a56231de7e99a167434e2e457c30
mazterziN <105863040+mazterziN@users.noreply.github.com>
2022-05-31 12:26:22 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -5,17 +5,17 @@ end)
 
 -- SONIC SPEED SIMULATOR
 if game.PlaceId == 9049840490 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/mazterziN/Scripts/main/mazterHub/SonicSpeedSimulator.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mazterziN/Hub/main/Scripts/AllStarTowerDefense.lua"))()
     
 -- ALL STAR TOWER DEFENSE
 elseif game.PlaceId == 4996049426 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/mazterziN/Scripts/main/mazterHub/AllStarTowerDefense.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mazterziN/Hub/main/Scripts/AllStarTowerDefense.lua"))()
 
 -- GRAND PIRATES
 elseif game.PlaceId == 9136292138 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/mazterziN/Scripts/main/mazterHub/GrandPirates.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mazterziN/Hub/main/Scripts/GrandPirates.lua"))()
 
 -- GENERAL
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/mazterziN/Scripts/main/mazterHub/General.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/mazterziN/Hub/main/Scripts/General.lua"))()
 end
