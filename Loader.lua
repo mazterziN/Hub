050113ca147bdd851c6555fd071e3f2829bc5f05
mazterziN <105863040+mazterziN@users.noreply.github.com>
2022-06-18 04:00:49 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -7,10 +7,6 @@ end)
 -- SONIC SPEED SIMULATOR
 if game.PlaceId == 9049840490 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/mazterziN/Hub/main/Scripts/AllStarTowerDefense.lua"))()
-    
--- ALL STAR TOWER DEFENSE
-elseif game.PlaceId == 4996049426 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/mazterziN/Hub/main/Scripts/AllStarTowerDefense.lua"))()
 
 -- GRAND PIRATES
 elseif game.PlaceId == 9136292138 then
