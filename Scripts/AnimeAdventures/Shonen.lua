1c214f57c5f5c80bc8b0bb7c71365ace4b79f297
mazterziN <105863040+mazterziN@users.noreply.github.com>
2022-08-26 03:54:44 -0400

    Update Shonen.lua

--- Scripts/AnimeAdventures/Shonen.lua
+++ Scripts/AnimeAdventures/Shonen.lua
@@ -66,64 +66,64 @@ local function PlaceUnits()
         if getgenv().map == "hueco" then
             if game:GetService("Workspace")["_wave_num"].Value < 3 then
                 local u1 = CFrame.new(-154.05145263671875, 132.66400146484375, -725.6512451171875) * CFrame.Angles(0, -0, -0)
-                Place1(getgenv().getUnitId[1], u1)
+                Place1(getgenv().getUnitId["bulma"], u1)
             end
             if game:GetService("Workspace")["_wave_num"].Value < 3 then
                 local u1 = CFrame.new(-188.1686248779297, 132.6639862060547, -761.1053466796875) * CFrame.Angles(0, -0, -0)
                 local u2 = CFrame.new(-187.72862243652344, 132.6639862060547, -759.0645751953125) * CFrame.Angles(0, -0, -0)
                 local u3 = CFrame.new(-187.40025329589844, 132.6639862060547, -757.1781005859375) * CFrame.Angles(0, -0, -0)
-                Place3(getgenv().getUnitId[2], u1, u2, u3)
+                Place3(getgenv().getUnitId["erwin"], u1, u2, u3)
             end
             if game:GetService("Workspace")["_wave_num"].Value > 2 and game:GetService("Workspace")["_wave_num"].Value < 6 then
                 local u1 = CFrame.new(-190.58680725097656, 132.66400146484375, -760.87353515625) * CFrame.Angles(0, -0, -0)
                 local u2 = CFrame.new(-190.31472778320312, 132.66400146484375, -758.8143310546875) * CFrame.Angles(0, -0, -0)
                 local u3 = CFrame.new(-189.9259033203125, 132.66400146484375, -756.8162841796875) * CFrame.Angles(0, -0, -0)
                 local u4 = CFrame.new(-189.41905212402344, 132.66400146484375, -754.8712158203125) * CFrame.Angles(0, -0, -0)
-                Place4(getgenv().getUnitId[3], u1, u2, u3, u4)
+                Place4(getgenv().getUnitId["juuzou"], u1, u2, u3, u4)
             end
             if game:GetService("Workspace")["_wave_num"].Value > 8 and game:GetService("Workspace")["_wave_num"].Value < 11 then
                 local u1 = CFrame.new(-192.7289276123047, 132.66400146484375, -760.8720703125) * CFrame.Angles(0, -0, -0)
                 local u2 = CFrame.new(-192.354248046875, 132.66400146484375, -758.7332763671875) * CFrame.Angles(0, -0, -0)
                 local u3 = CFrame.new(-191.98509216308594, 132.66400146484375, -756.726806640625) * CFrame.Angles(0, -0, -0)
-                Place3(getgenv().getUnitId[4], u1, u2, u3)
+                Place3(getgenv().getUnitId["kisuke"], u1, u2, u3)
             end
         -- TOKYO GHOUL
         elseif getgenv().map == "tokyoghoul" then
             if game:GetService("Workspace")["_wave_num"].Value < 3 then
                 local u1 = CFrame.new(-3016.866943359375, 58.58513259887695, -40.86863708496094) * CFrame.Angles(0, -0, -0)
-                Place1(getgenv().getUnitId[1], u1)
+                Place1(getgenv().getUnitId["bulma"], u1)
             end
             if game:GetService("Workspace")["_wave_num"].Value < 3 then
                 local u1 = CFrame.new(-3000.849609375, 58.58513641357422, -64.70999908447266) * CFrame.Angles(0, -0, -0)
                 local u2 = CFrame.new(-3000.864501953125, 58.58513641357422, -62.40230178833008) * CFrame.Angles(0, -0, -0)
                 local u3 = CFrame.new(-3000.8408203125, 58.58513641357422, -60.03608703613281) * CFrame.Angles(0, -0, -0)
-                Place3(getgenv().getUnitId[2], u1, u2, u3)
+                Place3(getgenv().getUnitId["erwin"], u1, u2, u3)
             end
             if game:GetService("Workspace")["_wave_num"].Value > 2 and game:GetService("Workspace")["_wave_num"].Value < 6 then
                 local u1 = CFrame.new(-2997.535400390625, 58.58513641357422, -68.66865539550781) * CFrame.Angles(0, 1.5707963705062866, 0)
                 local u2 = CFrame.new(-2997.430419921875, 58.58513641357422, -66.29708862304688) * CFrame.Angles(0, 1.5707963705062866, 0)
                 local u3 = CFrame.new(-2997.5947265625, 58.58513641357422, -63.99013900756836) * CFrame.Angles(0, 1.5707963705062866, 0)
                 local u4 = CFrame.new(-2997.591064453125, 58.58513641357422, -61.75052261352539) * CFrame.Angles(0, 1.5707963705062866, 0)
-                Place4(getgenv().getUnitId[3], u1, u2, u3, u4)
+                Place4(getgenv().getUnitId["juuzou"], u1, u2, u3, u4)
             end
         -- NAMEK
         elseif getgenv().map == "namek" then
             if game:GetService("Workspace")["_wave_num"].Value < 4 then
                 local u1 = CFrame.new(-2914.83544921875, 91.80620574951172, -737.7935180664062) * CFrame.Angles(3.1415927410125732, -0, -3.1415927410125732)
-                Place1(getgenv().getUnitId[1], u1)
+                Place1(getgenv().getUnitId["bulma"], u1)
             end
             if game:GetService("Workspace")["_wave_num"].Value < 4 then
                 local u1 = CFrame.new(-2940.32861328125, 91.80620574951172, -706.9010009765625) * CFrame.Angles(3.1415927410125732, -0, -3.1415927410125732)
                 local u2 = CFrame.new(-2942.4169921875, 91.80620574951172, -706.8063354492188) * CFrame.Angles(3.1415927410125732, -0, -3.1415927410125732)
                 local u3 = CFrame.new(-2944.421875, 91.80620574951172, -707.0632934570312) * CFrame.Angles(3.1415927410125732, -0, -3.1415927410125732)
-                Place3(getgenv().getUnitId[2], u1, u2, u3)
+                Place3(getgenv().getUnitId["erwin"], u1, u2, u3)
             end
             if game:GetService("Workspace")["_wave_num"].Value > 3 and game:GetService("Workspace")["_wave_num"].Value < 7 then
                 local u1 = CFrame.new(-2939.342529296875, 91.80620574951172, -703.744140625) * CFrame.Angles(3.1415927410125732, -0, -3.1415927410125732)
                 local u2 = CFrame.new(-2941.7685546875, 91.80620574951172, -703.731689453125) * CFrame.Angles(3.1415927410125732, -0, -3.1415927410125732)
                 local u3 = CFrame.new(-2943.829345703125, 91.80620574951172, -704.05126953125) * CFrame.Angles(3.1415927410125732, -0, -3.1415927410125732)
                 local u4 = CFrame.new(-2945.931396484375, 91.80620574951172, -704.1384887695312) * CFrame.Angles(3.1415927410125732, -0, -3.1415927410125732)
-                Place4(getgenv().getUnitId[3], u1, u2, u3, u4)
+                Place4(getgenv().getUnitId["juuzou"], u1, u2, u3, u4)
             end
         end
     end)
