3dfa015f180984535d9caff7953f9c12fcfb2422
mazterziN <105863040+mazterziN@users.noreply.github.com>
2022-09-04 21:57:18 -0400

    Update UnitsID.lua

--- Scripts/AnimeAdventures/UnitsID.lua
+++ Scripts/AnimeAdventures/UnitsID.lua
@@ -3,11 +3,14 @@ local buff = ""
 local primary = ""
 local secondary = ""
 
+-- MAZTER
 if game.Players.LocalPlayer.UserId == 1340755305 then
     money = "{780300c0-549c-49fb-b06d-22a47675165c}"
     buff = "{8c4e6861-5948-4d4f-b0d7-e31e8126d4e7}"
-    primary = "{519a4f24-0743-4a08-a529-6b242bf5cdf4}"
-    secondary = "{0d4633e5-123f-4fb4-ae5e-a9c32ff8c732}"
+    primary = "{629b8451-5bc7-41ec-8a43-420e79d2bf8f}"
+    secondary = "{672f9c81-f1a3-4635-b91c-09f6300a0f2e}"
+
+-- LUIPY
 elseif game.Players.LocalPlayer.UserId == 1022210737 then
     money = "{de44d3f9-a315-4132-9cd8-bee28c72b74c}"
     buff = "{433ed73e-0874-4252-a5ae-3631c4956ff6}"
